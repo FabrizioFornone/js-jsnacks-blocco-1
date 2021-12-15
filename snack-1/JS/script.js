d69f47c5ae6bd9c3e0096508f4a72fa1df43830d
FabrizioFornone <fabrizio.fornone@hotmail.it>
2021-12-15 19:45:13 +0100

    Snack-1 Done (some fixes)

--- snack-1/JS/script.js
+++ snack-1/JS/script.js
@@ -1,17 +1,24 @@
+/*Il software deve chiedere per 10 volte all’utente
+ di inserire un numero. Il programma stampa la somma di tutti
+  i numeri inseriti */
+
 // Hook HTML
 
-const hookHtml = document.getElementById('hook');
+const hookHtml = document.getElementById("hook");
 
 // Ciclo for x generare 10 prompt
 
 let numRequest = 0;
 
-for (let i = 0; i < 10; i++) {
-  numRequest += parseInt(prompt("Inserisci un numero"));
-}
+let sum = 0;
 
-console.log(numRequest);
+for (let i = 0; i < 10; i++) {
+  numRequest = parseInt(prompt("Inserisci un numero"));
+  console.log(numRequest);
+  sum += numRequest;
+  console.log(sum);
+}
 
 // Stampo in HTML
 
-hookHtml.innerHTML = numRequest;
+hookHtml.innerHTML = `La somma dei numeri inseriti è: <strong> ${sum} </strong>`;
