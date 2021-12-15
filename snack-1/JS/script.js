a7fb753597dc691dff7f0b60a2335a47c66196cb
FabrizioFornone <fabrizio.fornone@hotmail.it>
2021-12-15 14:41:03 +0100

    Snack-1 Done

---
+++ snack-1/JS/script.js
@@ -0,0 +1,17 @@
+// Hook
+
+const hookHtml = document.getElementById('hook');
+
+// Ciclo for x generare 10 prompt
+
+let numRequest = 0;
+
+for (let i = 0; i < 10; i++) {
+  numRequest += parseInt(prompt("Inserisci un numero"));
+}
+
+console.log(numRequest);
+
+// Stampo in HTML
+
+hookHtml.innerHTML = numRequest;
