30cd254a1344c0c192f4180d12bab69c2f2cc152
FabrizioFornone <fabrizio.fornone@hotmail.it>
2021-12-15 20:15:44 +0100

    Snack 2 Done (small changes)

--- snack-2/JS/script.js
+++ snack-2/JS/script.js
@@ -6,13 +6,25 @@
 
 const hookHtml = document.getElementById("hook");
 
-let numuser = parseInt(prompt("Inserisci un numero"));
+// Dichiaro le variabili
+
+let numUser = prompt("Inserisci un numero");
 
 let cube;
 
-for (let i = 0; i < numuser; i++) {
-  cube = Math.pow(i, 3);
-  console.log(cube);
-  //stampo in HTML
-  hookHtml.innerHTML += `<br/>${cube}`;
+// Check se è un numero
+
+if (isNaN(numUser)) {
+  alert("Inserisci un numero!");
+} else {
+  // Risposta su HTML
+  hookHtml.innerHTML += `Bene hai inserito: ${numUser}, ti darò il cubo di ${numUser} numeri! `;
+  // Ciclo for ripetuto "numUser" volte
+  for (let i = 1; i <= numUser; i++) {
+    // cube è il cubo di i
+    cube = Math.pow(i, 3);
+    console.log(cube);
+    //stampo in HTML cube
+    hookHtml.innerHTML += `<br/>${cube}`;
+  }
 }
