15d8d217011a35be7a196db92d831d631e32ed1f
FabrizioFornone <fabrizio.fornone@hotmail.it>
2021-12-15 22:11:07 +0100

    Snack-3 Done (small changes)

--- snack-4/JS/script.js
+++ snack-4/JS/script.js
@@ -5,14 +5,33 @@
 
 const hookHtml = document.getElementById("hook");
 
+// prompt di richiesta
+
 let numRequest = prompt(
   "Per favore, digita un numero di 4 cifre, farò la somma di queste per te"
 );
 
+// dichiarazione di sum
+
 let sum = 0;
 
-for (i = 0; i < numRequest.length; i++) {
-  sum += parseInt(numRequest[i]);
-}
+// Check se è un numero
 
-hookHtml.innerHTML = sum;
+if (isNaN(numRequest)) {
+  alert("Non hai inserito un numero, ricarica la pagina");
+}
+// Check se è un numero a 4 cifre esatte
+else if (numRequest.length !== 4) {
+  alert("Inserisci esattamente 4 cifre! Ricarica la pagina");
+}
+// Esegui se l'informazione inserita è valida
+else {
+  // Ciclo for che dura quanti sono i caratteri della stringa inserita
+  for (i = 0; i < numRequest.length; i++) {
+    /* sum è una variabile che somma sempre i valori inseriti
+   con numberRequest, convertendoli in numeri */
+    sum += parseInt(numRequest[i]);
+  }
+  //Stampa sum in HTML
+  hookHtml.innerHTML = `Ecco a te la somma <strong> ${sum} </strong> `;
+}
